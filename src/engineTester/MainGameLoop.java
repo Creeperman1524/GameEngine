1141dce1269bf2a89f664cadb55b39685dd95a90
Creeperman1524 <38088724+Creeperman1524@users.noreply.github.com>
2021-02-20 16:16:48 -0500

    Texturing implementation added
    Models can now be rendered with textures on them

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,11 +1,14 @@
 package engineTester;
 
 import org.lwjgl.opengl.Display;
+
+import models.RawModel;
+import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
 import renderEngine.Renderer;
 import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class MainGameLoop {
     public static void main(String[] args) {
@@ -16,16 +19,24 @@ public class MainGameLoop {
         Renderer renderer = new Renderer();
         StaticShader shader = new StaticShader();
 
-        float[] vertices = { 
-            -0.5f, 0.5f, 0, //V0
-            -0.5f, -0.5f, 0, //V1
-            0.5f, -0.5f, 0, //V2
-            0.5f, 0.5f, 0 //V3
+        float[] vertices = { -0.5f, 0.5f, 0, // V0
+                -0.5f, -0.5f, 0, // V1
+                0.5f, -0.5f, 0, // V2
+                0.5f, 0.5f, 0 // V3
         };
 
         int[] indicies = { 0, 1, 3, 3, 1, 2 };
 
-        RawModel model = loader.loadToVao(vertices, indicies);
+        float[] textureCoords = {
+            0,0,
+            0,1,
+            1,1,
+            1,0
+        };
+
+        RawModel model = loader.loadToVao(vertices, textureCoords, indicies);
+        ModelTexture texture = new ModelTexture(loader.loadTexture("smile"));
+        TexturedModel texturedModel = new TexturedModel(model, texture);
 
         while (!Display.isCloseRequested()) {
             renderer.prepare();
@@ -33,7 +44,7 @@ public class MainGameLoop {
             // game logic
             // render
             shader.start();
-            renderer.render(model);
+            renderer.render(texturedModel);
             shader.stop();
             DisplayManager.updateDisplay();
 
